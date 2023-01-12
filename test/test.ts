82004dc5e286b89ec0c06d121637171e43dfe551
hsiaofongw <hsiaofong.w@gmail.com>
2023-01-12 12:53:58 +0800

    Update

--- test/test.ts
+++ test/test.ts
@@ -1,1 +1,3 @@
-import { ConcatTuple, MakeTuple } from "type-functions";
+import { ConcatTuple, MakeTuple, SortNumsAscending } from "type-functions";
+
+type A = SortNumsAscending<[3, 1, 5, 7, 2, 8]>;
